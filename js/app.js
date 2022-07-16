d8eb7cf46aa86c9316a3a438e1ec921a3fc36f2e
iliassel19 <89216825+iliassel19@users.noreply.github.com>
2022-07-16 15:48:46 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,16 +7,19 @@ tl.from(".navigation__item", {
   stagger: 0.2,
 });
 
+gsap.registerPlugin(ScrollTrigger);
+
 const menu = document.querySelector(".navigation__burger");
 const menuLines = document.querySelectorAll(".navigation__burger-line");
 const menuList = document.querySelector(".navigation__list");
 const navigationItems = document.querySelector(".navigation__list");
-
+console.log(document);
 // Mobile navigation handler
 const menuOpen = function () {
   menu.addEventListener("click", function () {
     menuLines.forEach((line) => line.classList.toggle("active"));
     menuList.classList.toggle("active");
+    menu.classList.toggle("active");
   });
 };
 menuOpen();
@@ -50,3 +53,64 @@ const smoothScroll = function (e) {
 };
 
 smoothScroll();
+
+// nIntersection observer
+
+let options = {
+  root: null,
+  rootMargin: "-25px",
+  threshold: 0.2,
+};
+const showHeadingWhy = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.add("active");
+  });
+};
+let observer = new IntersectionObserver(showHeadingWhy, options);
+const headingWhy = document.querySelector(".why__heading");
+observer.observe(headingWhy);
+
+options = {
+  root: null,
+  rootMargin: "-20px",
+  threshold: 0.25,
+};
+const whyContainer1 = document.querySelector(".why__container-1");
+const whyHeadingTertiary1 = document.querySelector(".heading-tertiary-1");
+const whyText1 = document.querySelector(".why__text-1");
+const whyImg1 = document.querySelector(".why__img-1");
+const showWhyBox1 = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    whyHeadingTertiary1.classList.add("active");
+    whyText1.classList.add("active");
+    whyImg1.classList.add("active");
+  });
+};
+
+observer = new IntersectionObserver(showWhyBox1, options);
+observer.observe(whyContainer1);
+
+const whyContainer2 = document.querySelector(".why__container-2");
+const whyHeadingTertiary2 = document.querySelector(".heading-tertiary-2");
+const whyText2 = document.querySelector(".why__text-2");
+const whyImg2 = document.querySelector(".why__img-2");
+const showWhyBox2 = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    console.log(entry);
+    whyHeadingTertiary2.classList.add("active");
+    whyText2.classList.add("active");
+    whyImg2.classList.add("active");
+  });
+};
+
+options = {
+  root: null,
+  rootMargin: "-120px",
+  threshold: 0.25,
+};
+observer = new IntersectionObserver(showWhyBox2, options);
+observer.observe(whyContainer2);
